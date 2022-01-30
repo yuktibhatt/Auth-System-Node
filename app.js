2fae14c19c28d6886667092139f3ef01afb14548
yuktibhatt <bhattyukti5@gmail.com>
2022-01-30 16:52:40 +0545

    pw encryption and jwt tokens

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 require('./config/database').connect();
 const express = require('express');
 const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const user = require("./model/user");
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
@@ -12,7 +15,8 @@ app.get("/", (req,res) => {
 });
 
 app.post("/register", async (req,res) => {
-    const { firstname, lastname, email, password } = req.body;
+    try {
+        const { firstname, lastname, email, password } = req.body;
 
     if( !(email && password && firstname && lastname)) {
         res.status(400).send('All fields are required');
@@ -23,7 +27,37 @@ app.post("/register", async (req,res) => {
     if(existingUser) {
         res.status(401).send('User already exists!');
     }
+
+    const myEncPassword = await bcrypt.hash(password, 10)
+
+    const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password: myEncPassword
+    });
+
+    //token creation
+    const token = jwt.sign(
+        {user_id: user._id, email}, 
+        process.env.SECRET_KEY,
+        {
+            expiresIn: "2h"
+        }
+    )
+    user.token = token
+    //update or not in db ?
+
+    //handle the pw situation 
+
+    res.status(201).json(user);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
 });
+
 
 
 module.exports = app;

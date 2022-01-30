58904201f2096e7fa72926b466b186e0c3590692
yuktibhatt <bhattyukti5@gmail.com>
2022-01-30 21:05:20 +0545

    login flow

--- app.js
+++ app.js
@@ -52,13 +52,43 @@ app.post("/register", async (req,res) => {
     user.password = undefined;
 
     res.status(201).json(user);
-        
     } catch (error) {
-        console.log(error);
-        
+        console.log(error); 
     }
 });
 
+app.post("/login", async (req,res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!(email && password)){
+            res.status(400).send("Field is missing")
+        }
+        const user = await User.findOne({email})
+
+        // if(!user){
+        //     res.status(400).send("You are not registered yet")
+        // }
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign(
+                {user_id : user._id, email },
+                process.env.SECRET_KEY,
+                {
+                    expiresIn: "2h"
+                }
+            )
+            user.token = token
+            user.password = undefined
+            res.status(200).json(user)
+
+        }
+        res.status(400).send("Email or password is incorrect");
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 module.exports = app;
